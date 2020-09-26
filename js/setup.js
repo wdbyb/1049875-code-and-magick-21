bf628212f931b1e2bec903efb9c3582bd7c2b768
Вини <yvinivitin@gmail.com>
2020-09-26 14:11:05 +0300

    Переименованы переменные; использован forEach

--- js/setup.js
+++ js/setup.js
@@ -1,13 +1,13 @@
-'use strict';
+`use strict`;
 
 const FIRST_NAMES = [`Иван`, `Хуан Себастьян`, `Мария`, `Кристоф`, `Виктор`, `Юлия`, `Люпита`, `Вашингтон`];
 const LAST_NAMES = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топольницкая`, `Нионго`, `Ирвинг`];
 const COAT_COLORS = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
 const EYES_COLORS = [`black`, `red`, `blue`, `yellow`, `green`];
 const MAX_WIZARDS = 4;
-const setup = document.querySelector(`.setup`);
-const wizardTemplate = document.querySelector(`#similar-wizard-template`).content.querySelector(`div`);
-const wizardsList = setup.querySelector(`.setup-similar-list`);
+const setupElement = document.querySelector(`.setup`);
+const wizardTemplateElement = document.querySelector(`#similar-wizard-template`).content.querySelector(`div`);
+const wizardsListElement = setupElement.querySelector(`.setup-similar-list`);
 const wizards = [];
 
 const getRandom = function (max) {
@@ -15,7 +15,7 @@ const getRandom = function (max) {
 };
 
 const renderWizard = function (wizard) {
-  const wizardElement = wizardTemplate.cloneNode(true);
+  const wizardElement = wizardTemplateElement.cloneNode(true);
 
   wizardElement.querySelector(`.setup-similar-label`).textContent = wizard.name;
   wizardElement.querySelector(`.wizard-coat`).style.fill = wizard.coatColor;
@@ -24,7 +24,7 @@ const renderWizard = function (wizard) {
   return wizardElement;
 };
 
-setup.classList.remove(`hidden`);
+setupElement.classList.remove(`hidden`);
 
 for (let i = 0; i < MAX_WIZARDS; i++) {
   wizards.push({
@@ -36,10 +36,8 @@ for (let i = 0; i < MAX_WIZARDS; i++) {
 
 const fragment = document.createDocumentFragment();
 
-for (let i = 0; i < MAX_WIZARDS; i++) {
-  fragment.appendChild(renderWizard(wizards[i]));
-}
+wizards.forEach(element => fragment.appendChild(renderWizard(element)));
 
-wizardsList.appendChild(fragment);
+wizardsListElement.appendChild(fragment);
 
 document.querySelector(`.setup-similar`).classList.remove(`hidden`);
